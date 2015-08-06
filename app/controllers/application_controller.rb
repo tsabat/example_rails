cdb8f1e32a86515ac3025e9b1c92526bc09a72f2
Timothy Sabat <timothy.sabat@gmail.com>
2015-08-06 10:43:51 -0700

    add cache headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   def say_hi
     respond_to do |format|
+      cache_buster
+
       file = Rails.root.join('version.txt')
       if File.exist?(file)
         rslt = File.read(file)
@@ -13,5 +15,11 @@ class ApplicationController < ActionController::Base
       end
       format.html { render text: "version: #{rslt}<br>hostname: #{`hostname`}" }
     end
+  end
+
+  def cache_buster
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
   end
 end
